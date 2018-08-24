c82b7214e1b7d8ff443b9bede97391c403e85345
Prashant Mudakavi <prashant.mudakavi@rackspace.com>
2018-08-24 15:51:58 -0500

    rebase

--- tests/unit/procedures/test_stress.py
+++ tests/unit/procedures/test_stress.py
@@ -1,6 +1,5 @@
 import mock
 
-from mercury.common.helpers.cli import CLIResult
 from mercury_agent.procedures.stress import Stress
 
 from tests.unit import base
@@ -10,11 +9,7 @@ class TestStress(base.MercuryAgentUnitTest):
 
     @mock.patch('mercury_agent.hardware.raid.interfaces.megaraid.stress.cli')
     def stress_test(self, mock_cli):
-        mock_cli.run.return_value = CLIResult('', '', 0)
         mock_cli.find_in_path.return_value = '/usr/bin/stress'
-
         s = Stress()
-
         assert s.start(seconds=60)
-
-
+        mock_cli.find_in_path.return_value = None
