dec2ba58831c929f70ccb9154ed20bc48f66f962
Prashant Mudakavi <prashant.mudakavi@rackspace.com>
2018-09-10 11:22:00 -0500

    fixes per PR comments

--- mercury_agent/procedures/stress.py
+++ mercury_agent/procedures/stress.py
@@ -7,20 +7,7 @@ from mercury_agent.capabilities import capability
 from mercury.common.helpers import cli
 
 
-# Request the Stress binary
-# http://people.seas.harvard.edu/~apw/stress/
-STRESS = '/usr/bin/stress'
-
-
-def get_logger(label='PyStress Logger'):
-    """
-    Create and returns simple Logger
-    """
-    fmt = '%(asctime)s - %(levelname)s - %(message)s'
-    logging.basicConfig(format=fmt)
-    logger = logging.getLogger(label)
-    logger.setLevel(getattr(logging, 'INFO'))
-    return logger
+log = logging.getLogger(__name__)
 
 
 class Stress(object):
@@ -28,23 +15,20 @@ class Stress(object):
     Class for running our stress commands.
     """
 
-    def __init__(self, logger=None):
+    def __init__(self, stress_path="/usr/bin/stress"):
         """
         Make sure the stress binary is found
         """
-        if not path.exists(STRESS):
-            raise Exception('Stress binary not found at {}.'.format(STRESS))
-
-        if logger is None:
-            self.logger = get_logger()
-        else:
-            self.logger = logger
+        self.stress_path = stress_path
+        if not path.exists(stress_path):
+            raise Exception('Stress binary not found at '
+                            '{}.'.format(self.stress_path))
 
     def get_system_memory(self):
         """
         Gets the amount of system memory in a server.
         """
-        self.logger.info('Getting the system memory')
+        log.info('Getting the system memory')
         cmd = 'grep MemFree /proc/meminfo'
         result = cli.run(cmd)
 
@@ -57,26 +41,21 @@ class Stress(object):
         """
         The way the stress binary behaves makes it hard to kill
         """
-        self.logger.error('Killing Stress Process.')
+        log.error('Killing Stress Process.')
         system('killall stress')
-
-    @staticmethod
-    def get_number_of_cpu_cores():
-        """
-        Gets the number of cores on a machine.
-        """
-        return cpu_count()
 
     def cpu_stress(self, timeout):
         """
         Stress test the cpu on all cores.
         """
-        self.logger.info('Stress testing the cpu for %s seconds.' % timeout)
-        cmd = '{0} --cpu {1} -t {2}'.format(STRESS,
-                                            self.get_number_of_cpu_cores(),
+        log.info('Stress testing the cpu for %s seconds.' % timeout)
+        cmd = '{0} --cpu {1} -t {2}'.format(self.stress_path,
+                                            cpu_count(),
                                             timeout)
-        self.logger.info('Executing: {}'.format(cmd))
-        return cli.run(cmd)
+        log.info('Executing: {}'.format(cmd))
+        result = cli.run(cmd)
+        if result.returncode:
+            self.killall()
 
     def memory_stress(self, timeout, amount=None):
         """
@@ -93,46 +72,31 @@ class Stress(object):
             if not amount:
                 amount = '1024M'
 
-        self.logger.info('Stressing memory for %s seconds at %s.' %
+        log.info('Stressing memory for %s seconds at %s.' %
                          (timeout, amount))
-        cmd = '{0} --vm 1 --vm-bytes {1} --vm-hang 5 -t {2}'.format(STRESS,
-                                                                   amount,
-                                                                   timeout)
-        self.logger.info('Executing: {}'.format(cmd))
-        return cli.run(cmd)
-
-    def log_stress_result(self, result):
-        """
-        logging stress results.
-        """
-        if result.returncode == 0:
-            for line in result.split('\n'):
-                if line:
-                    self.logger.info(line)
-        else:
-            for line in result.stderr.split('\n'):
-                if line:
-                    self.logger.error(line)
+        cmd = '{0} --vm 1 --vm-bytes {1} --vm-hang 5 -t {2}'.format(
+            self.stress_path, amount, timeout)
+        log.info('Executing: {}'.format(cmd))
+        result = cli.run(cmd)
+        if result.returncode:
             self.killall()
 
-    def start(self, seconds=900):
-        """
-        Start the stress processes and run for a number of seconds.
-        """
 
-        self.logger.info('Running Stress for {} seconds'.format(seconds))
-
-        self.log_stress_result(self.cpu_stress(timeout=seconds))
-
-        self.log_stress_result(self.memory_stress(timeout=seconds))
-
-
-@capability('stress_test',
-            description='Run stress on chassis for specified amount of time',
+@capability('memory_stress_test',
+            description='Run stress on memory for specified amount of time',
             kwarg_names=['stress_seconds'], serial=False, timeout=3600)
-def stress_test(stress_seconds=60):
-    log = get_logger()
+def memory_stress_test(stress_seconds=60):
     log.info('Starting stress test on CPU and Memory for '
              '{} seconds'.format(stress_seconds))
-    stress_tester = Stress(logger=log)
-    stress_tester.start(stress_seconds)
+    stress_tester = Stress()
+    stress_tester.memory_stress(stress_seconds)
+
+
+@capability('cpu_stress_test',
+            description='Run stress on cpu for specified amount of time',
+            kwarg_names=['stress_seconds'], serial=False, timeout=3600)
+def cpu_stress_test(stress_seconds=60):
+    log.info('Starting stress test on CPU and Memory for '
+             '{} seconds'.format(stress_seconds))
+    stress_tester = Stress()
+    stress_tester.cpu_stress(stress_seconds)
--- tests/unit/procedures/test_stress.py
+++ tests/unit/procedures/test_stress.py
@@ -1,5 +1,6 @@
 import mock
 
+from mercury.common.helpers.cli import CLIResult
 from mercury_agent.procedures.stress import Stress
 
 from tests.unit import base
@@ -8,8 +9,34 @@ from tests.unit import base
 class TestStress(base.MercuryAgentUnitTest):
 
     @mock.patch('mercury_agent.procedures.stress.cli')
-    def stress_test(self, mock_cli):
+    def memory_stress_test(self, mock_cli):
         mock_cli.find_in_path.return_value = '/usr/bin/stress'
         s = Stress()
-        assert s.start(seconds=60)
+        s.memory_stress = mock.Mock()
+        s.memory_stress.assert_called_with(seconds=60)
         mock_cli.find_in_path.return_value = None
+
+    @mock.patch('mercury_agent.procedures.stress.cli')
+    def cpu_stress_test(self, mock_cli):
+        mock_cli.find_in_path.return_value = '/usr/bin/stress'
+        s = Stress()
+        s.cpu_stress = mock.Mock()
+        s.cpu_stress.assert_called_with(seconds=60)
+        mock_cli.find_in_path.return_value = None
+
+    @mock.patch('mercury_agent.procedures.stress.cli')
+    def get_system_memory(self, mock_cli):
+        mock_cli.run.return_value = CLIResult('', '', 0)
+        mock_cli.find_in_path.return_value = '/proc/meminfo'
+        s = Stress()
+
+        assert s.get_system_memory() == '1234G'
+        mock_cli.run.return_value = CLIResult('1234G', '', 0)
+
+    @mock.patch('mercury_agent.procedures.stress.cli')
+    def kill_all(self, mock_cli):
+        mock_cli.run.return_value = CLIResult('', '', 0)
+        s = Stress()
+
+        assert s.killall() == ''
+
